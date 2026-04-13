8d400dba056c990497bf564932d30aa939f2b9e9
Ethin Issac Gerald <ethinissacgerald@gmail.com>
2026-04-13 09:42:18 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import joblib
 
 # Load data
@@ -30,9 +31,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# Save model
+# Accuracy
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
+print(f"Model Accuracy: {acc*100:.2f}%")
+
+# Save everything
 joblib.dump(model, "model/model.pkl")
 joblib.dump(le, "model/encoder.pkl")
 joblib.dump(X.columns.tolist(), "model/features.pkl")
 
-print("Model trained and saved ")
+# Save dataset for visualization
+df.to_csv("model/processed_data.csv", index=False)
+
+print("Model trained and saved ✅")
